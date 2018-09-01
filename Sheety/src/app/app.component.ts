cf7732f8cf6dbb199f05dd63c266ca88ff5b1988
Wei Liu <weil@zillowgroup.com>
2018-08-31 18:06:02 -0700

    Add code to grab header columns

--- Sheety/src/app/app.component.ts
+++ Sheety/src/app/app.component.ts
@@ -9,6 +9,8 @@ const template = require('./app.component.html');
 })
 export default class AppComponent {
     welcomeMessage = 'Welcome';
+    app = this;
+    headerJson = "";
 
     async run() {
         try {
@@ -17,17 +19,30 @@ export default class AppComponent {
                  * Insert your Excel code here
                  */
                 const range = context.workbook.getSelectedRange();
+                const sheet = context.workbook.worksheets.getActiveWorksheet();
+                const headerRange = sheet.getRange('A1:Z1')
+                headerRange.load('values')
+                await context.sync()
 
-            // Read the range address
-            range.load('address');
+                // Get the header values and trim away the empty cells
+                const headers = headerRange
+                .values
+                .filter((v) => v.length > 0)
+                .map((v) => v.filter(w => w !== '' && w !== false))
+                const headersJson = JSON.stringify(headers)
+                console.log(`headers: ${headersJson}`)
+                this.headerJson = headersJson
 
-            // Update the fill color
-            range.format.fill.color = 'yellow';
+                // Read the range address
+                range.load('address');
 
-            await context.sync();
-            console.log(`The range address was ${range.address}.`);
+                // Update the fill color
+                range.format.fill.color = 'yellow';
+
+                await context.sync();
+                console.log(`The range address was ${range.address}.`);
             });
-        } catch(error) {
+        } catch (error) {
             OfficeHelpers.UI.notify(error);
             OfficeHelpers.Utilities.log(error);
         };
